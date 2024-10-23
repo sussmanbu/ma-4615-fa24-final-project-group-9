e5430a87de69a4151e759623c211df55c62dfdd5
lirachelbu <lirachel@bu.edu#>
2024-10-23 09:53:01 -0400

    added dataset

--- scripts/load_and_clean_data.R
+++ scripts/load_and_clean_data.R
@@ -22,9 +22,29 @@ all_data %>%
 names(all_data)
 
 ## CLEAN the data
-loan_data_clean <- loan_data |>
-  pivot_longer(2:5, names_to = "group", values_to = "refusal_rate")
+missing_values <- colSums(is.na(all_data))
+print(missing_values)
+all_data <- all_data[, -which(names(all_data) == "...24")]
 
-write_rds(loan_data_clean, file = here::here("dataset", "loan_refusal_clean.rds"))
+# Convert YEAR to numeric if it's not already
+all_data$YEAR <- as.numeric(all_data$YEAR)
+
+# Convert categorical variables to factors
+categorical_vars <- c("SEX", "SEX_LABEL", "ETH_GROUP", "ETH_GROUP_LABEL", 
+                      "RACE_GROUP", "RACE_GROUP_LABEL", "VET_GROUP", 
+                      "VET_GROUP_LABEL", "NAICS2017", "NAICS2017_LABEL")
+all_data[categorical_vars] <- lapply(all_data[categorical_vars], as.factor)
+
+##years column
+unique(all_data$YEAR)
+
+
+
+
+
+
+
+
+
 
 

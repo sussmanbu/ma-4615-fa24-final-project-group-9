f119097ae872c4a63ed84fa5ad7cb6550ff4cc26
#Wdhfn# <#wodheifn@gmail.com#>
2024-10-28 16:21:54 -0400

    minor changes

--- scripts/load_and_clean_data.R
+++ scripts/load_and_clean_data.R
@@ -1,7 +1,3 @@
-# This file is purely as an example.
-# Note, you may end up creating more than one cleaned data set and saving that
-# to separate files in order to work on different aspects of your project
-
 library(tidyverse)
 
 # This part loads the data.
